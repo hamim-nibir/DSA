3bdf254f15fafd331cda1bf99f10b3087b4b693d
hamim_nibir <nhnibir.te@gmail.com>
2025-05-20 22:32:35 +0600

    new leetcode easy problem solved with python

---
+++ Leetcode/findOccurance.py
@@ -0,0 +1,8 @@
+# problem : https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+
+haystack = input()
+needle = input()
+
+index = haystack.find(needle)
+
+print(index)
